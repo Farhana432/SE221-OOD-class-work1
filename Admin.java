0764cea0a31908fcaaf7fca1ad9875a80d7a639f
Farhana432 <76177755+Farhana432@users.noreply.github.com>
2021-02-21 18:44:38 +0600

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,15 @@
+
+package objectpassingexamples;
+
+
+public class Admin {
+    
+    void check_registration(Student S)
+    {
+        if(S.id == 3421 && S.total_dues < 100000)
+        {
+            System.out.println("Payment Clearance not done printed");
+        }
+    }
+    
+}
